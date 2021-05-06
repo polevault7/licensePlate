ceb7b417c1ece385c85756f40e97b6b3323f8b8c
polevault7 <whitevol@mail.ru>
2021-05-06 17:24:14 +0300

    message

--- src/test/java/com/example/number/NumberApplicationTests.java
+++ src/test/java/com/example/number/NumberApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.number;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class NumberApplicationTests {
+    @Autowired
+    MockMvc mockMvc;
 
-    @Test
-    void contextLoads() {
+    void crudTest() throws Exception {
+        mockMvc.perform(get("/numbers/1"))
+                .andExpect(
+                        content()
+                                .json("{\n" +
+                                        "    \"id\": 2,\n" +
+                                        "    \"aircraftId\": 2,\n" +
+                                        "    \"sourceCity\": \"KAZAN\",\n" +
+                                        "    \"destCity\": \"MOSCOW\",\n" +
+                                        "    \"departureTime\": 1608163200000,\n" +
+                                        "    \"arrivalTime\": 1500000,\n" +
+                                        "    \"journeyDuration\": 10000,\n" +
+                                        "    \"price\": 5000\n" +
+                                        "  }")
+                );
     }
-
 }
